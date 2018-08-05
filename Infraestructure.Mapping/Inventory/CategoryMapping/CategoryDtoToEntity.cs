7a1f56873a8e7317c62e514eee4ccae886294bda
Daniel Carbajal <daniel.carbajal@pucp.edu.pe>
2018-08-04 20:18:26 -0500

    Mapping moved

--- Infraestructure.Mapping/Inventory/CategoryMapping/CategoryDtoToEntity.cs
+++ Infraestructure.Mapping/Inventory/CategoryMapping/CategoryDtoToEntity.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Services.Implementation.Inventory
+namespace Infraestructure.Mapping.Inventory.CategoryMapping
 {
     public static class CategoryDtoToEntity
     {
